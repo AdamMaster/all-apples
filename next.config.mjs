4ebf8a156dc9786a95059d25ad819e196bf43d64
Lampezhev Adam <lampezhev86@gmail.com>
2024-05-17 17:30:09 +0300

    Обновил файл next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,18 +5,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Установите директорию вывода статических файлов
 	distDir: 'out',
-	// Опции экспорта
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
-		return {
-			'/': { page: '/' },
-			'/_not-found': { page: '/_not-found' },
-		}
-	}
 };
 
 export default nextConfig;
