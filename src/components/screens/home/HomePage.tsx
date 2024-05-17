e330a17cbeaadb6129bb54e331818cc073c7a823
Lampezhev Adam <lampezhev86@gmail.com>
2024-05-17 16:39:57 +0300

    Добавил текст

--- src/components/screens/home/HomePage.tsx
+++ src/components/screens/home/HomePage.tsx
@@ -11,6 +11,7 @@ export const HomePage = () => {
       <Catalog className={s.catalog} />
       <About className={s.about} />
       <DeliveryScreen className={s.delivery} />
+      Привет
     </div>
   )
 }
