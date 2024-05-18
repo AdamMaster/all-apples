c4e161ab813dafd5821e3999df3a81606bd9594d
Lampezhev Adam <lampezhev86@gmail.com>
2024-05-18 16:25:16 +0300

    изменил файл nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	distDir: 'out',
-};
+const nextConfig = {};
 
 export default nextConfig;
+
