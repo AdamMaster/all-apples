9397a210470bec98548a2d7a808397e4d43fe217
Lampezhev Adam <lampezhev86@gmail.com>
2024-05-23 17:05:18 +0300

    коммит

--- src/utils/mail.utils.ts
+++ src/utils/mail.utils.ts
@@ -2,15 +2,23 @@ import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
-const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.NODE_ENV !== 'development', // true
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD
-  }
-} as SMTPTransport.Options)
+let transport: nodemailer.Transporter
+
+if (typeof window === 'undefined') {
+  // Этот код будет выполняться только на сервере
+  transport = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.NODE_ENV !== 'development', // true
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD
+    }
+  } as SMTPTransport.Options)
+} else {
+  // Этот код будет выполняться только в браузере
+  console.log('Cannot create nodemailer transport in the browser')
+}
 
 type SendEmailDto = {
   sender: Mail.Address
@@ -18,14 +26,20 @@ type SendEmailDto = {
   subject: string
   message: string
 }
+
 export const sendEmail = async (dto: SendEmailDto) => {
   const { sender, recipients, subject, message } = dto
 
-  return await transport.sendMail({
-    from: sender,
-    to: recipients,
-    subject,
-    html: message,
-    text: message
-  })
+  if (transport) {
+    return await transport.sendMail({
+      from: sender,
+      to: recipients,
+      subject,
+      html: message,
+      text: message
+    })
+  } else {
+    // Если transport не определен, то возвращаем ошибку или обрабатываем ее по-другому
+    throw new Error('Nodemailer transport is not initialized')
+  }
 }
