4e20edc743bd68d1f935daa045b55b09ea462dda
Lampezhev Adam <lampezhev86@gmail.com>
2024-07-15 20:50:02 +0300

    добавил sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+	siteUrl: 'https://adams-apple.ru',
+	generateRobotsTxt: true,
+};
+
+module.exports = config;
--- next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://adams-apple.ru',
-	generateRobotsTxt: true, // (опционально) для создания robots.txt
-}
