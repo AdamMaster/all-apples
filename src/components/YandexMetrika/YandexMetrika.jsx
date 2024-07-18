28505145e50e544be360bc5fbe159c21f4377acb
Lampezhev Adam <lampezhev86@gmail.com>
2024-07-18 13:39:47 +0300

    coming

--- src/components/YandexMetrika/YandexMetrika.jsx
+++ src/components/YandexMetrika/YandexMetrika.jsx
@@ -20,7 +20,11 @@ export const YandexMetrica = ({ counterId }) => {
       </Script>
       <noscript>
         <div>
-          <img src={`https://mc.yandex.ru/watch/${counterId}`} style='position:absolute; left:-9999px;' alt='' />
+          <img
+            src={`https://mc.yandex.ru/watch/${counterId}`}
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=''
+          />
         </div>
       </noscript>
     </Fragment>
