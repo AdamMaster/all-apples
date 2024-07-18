e395e5172969645f792524fdbec4aea50517a101
Lampezhev Adam <lampezhev86@gmail.com>
2024-07-18 17:25:28 +0300

    yeah

--- src/components/GoogleAnalytics/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics/GoogleAnalytics.jsx
@@ -12,7 +12,7 @@ export const GoogleAnalytics = () => {
           src='https://www.googletagmanager.com/ns.html?id=GTM-KMTJZ6KF'
           height='0'
           width='0'
-          style='display:none;visibility:hidden'
+          style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
       </noscript>
     </Fragment>
