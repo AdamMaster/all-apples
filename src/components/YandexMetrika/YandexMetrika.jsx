16847a69f5db1ba002a4608a8cfea3a80ec9175d
Lampezhev Adam <lampezhev86@gmail.com>
2024-07-18 13:34:09 +0300

    comen

--- src/components/YandexMetrika/YandexMetrika.jsx
+++ src/components/YandexMetrika/YandexMetrika.jsx
@@ -6,26 +6,21 @@ export const YandexMetrica = ({ counterId }) => {
     <Fragment>
       <Script id='metrika-counter' strategy='afterInteractive'>
         {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-         
-            ym(${counterId}, "init", {
-                  defer: true,
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-                  webvisor:true
-            });`}
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(${counterId}, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}
       </Script>
       <noscript>
         <div>
-          <img
-            src={`https://mc.yandex.ru/watch/${counterId}`}
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt=''
-          />
+          <img src={`https://mc.yandex.ru/watch/${counterId}`} style='position:absolute; left:-9999px;' alt='' />
         </div>
       </noscript>
     </Fragment>
