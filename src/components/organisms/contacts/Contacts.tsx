1cf1317023e879c5adb33731a30349b9157fc33b
Lampezhev Adam <lampezhev86@gmail.com>
2024-10-31 22:35:27 +0300

    поменял карту на гугловскую

--- src/components/organisms/contacts/Contacts.tsx
+++ src/components/organisms/contacts/Contacts.tsx
@@ -53,10 +53,9 @@ export const Contacts: FC<Props> = ({ className }) => {
             </div>
             <div className={s.map}>
               <iframe
-                id='1'
-                src='https://yandex.ru/map-widget/v1/?um=constructor%3Ae1a9a48b3dfa281d53be18182d9997ac88c4afbded3e9671221a95a99262752f&amp;source=constructor'
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13727.384775799204!2d43.50054818197904!3d43.67256430239726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x405756cc386fd0b1%3A0x9d5dd376c498b3d9!2z0KfQtdGA0LrQtdGB0YHQutCw0Y8g0YPQuy4sIDc4LCDQkdCw0LrRgdCw0L0sINCa0LDQsdCw0YDQtNC40L3Qvi3QkdCw0LvQutCw0YDRgdC60LDRjyDQoNC10YHQvy4sIDM2MTUzMg!5e0!3m2!1sru!2sru!4v1730403213067!5m2!1sru!2sru'
                 width='100%'
-                height='400'
+                height='100%'
                 loading='lazy'
               ></iframe>
             </div>
