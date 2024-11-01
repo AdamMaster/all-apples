c770e49f387d93ae037ebe4c37c565a59cd48935
Lampezhev Adam <lampezhev86@gmail.com>
2024-11-01 15:15:35 +0300

    Добавил проверку

--- src/shared/api/fetchFeedback.ts
+++ src/shared/api/fetchFeedback.ts
@@ -10,14 +10,20 @@ export const fetchFeedback = async (data: FormData) => {
       Телефон: ${data.phone} <br>
       Сообщение: ${data.message}
     `
-  const response = await fetch('/api/feedback', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      subject: 'Обратный звонок',
-      message: message
+
+  try {
+    const response = await fetch('/api/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        subject: 'Обратный звонок',
+        message: message
+      })
     })
-  })
+    console.log('Письмо отправилось')
+  } catch (error) {
+    console.log('Письмо не отправилось:', error)
+  }
 }
