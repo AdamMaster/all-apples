ee89e44142143a4ac5182d6df442547d2be1943e
AdamMaster <lampezhev86@gmail.com>
2025-02-02 18:35:40 +0300

    обновил sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-	siteUrl: 'https://adams-apple.ru',
+	siteUrl: 'https://apple07.ru',
 	generateRobotsTxt: true,
+	additionalPaths: async () => [
+		{ loc: '/', changefreq: 'weekly', priority: 1.0 },
+		{ loc: '/partners', changefreq: 'monthly', priority: 0.8 },
+		{ loc: '/policy', changefreq: 'monthly', priority: 0.8 },
+	],
 };
 
 module.exports = config;
