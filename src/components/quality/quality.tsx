eef5fcee1956b22d3f9a9ac395765917b1add5e5
AdamMaster <lampezhev86@gmail.com>
2025-02-06 17:11:45 +0300

    изменил структу блока Quantity

--- src/components/quality/quality.tsx
+++ src/components/quality/quality.tsx
@@ -15,39 +15,29 @@ export const Quality: React.FC<Props> = ({ className }) => {
   const advantageItems: AdvantageItemType[] = [
     {
       id: 1,
-      title: 'Контроль перед отправкой',
-      description: 'Повторная проверка перед отправкой клиенту',
-      icon: '/images/quality-icon.svg'
+      title: 'Сбор и сортировка',
+      description: 'Ручная проверка и отбор только качественных яблок',
+      icon: '/images/boxes-icon.svg'
     },
     {
       id: 2,
-      title: 'Газовая среда',
-      description: 'Замедление созревания для продления свежести',
-      icon: '/images/gas-icon.svg'
-    },
-    {
-      id: 3,
       title: 'Хранение',
       description: 'Поддержание оптимальной температуры и влажности',
       icon: '/images/gradus-icon.svg'
     },
     {
+      id: 3,
+      title: 'Газовая среда',
+      description: 'Замедление созревания для продления свежести',
+      icon: '/images/gas-icon.svg'
+    },
+    {
       id: 4,
-      title: 'Сбор и сортировка',
-      description: 'Ручная проверка и отбор только качественных яблок',
-      icon: '/images/boxes-icon.svg'
+      title: 'Контроль перед отправкой',
+      description: 'Повторная проверка перед отправкой клиенту',
+      icon: '/images/quality-icon.svg'
     }
   ]
-
-  const [starsQuantity, setStarsQuantity] = React.useState(3)
-
-  React.useEffect(() => {
-    if (window.innerWidth <= 545) {
-      return setStarsQuantity(5)
-    } else {
-      return setStarsQuantity(3)
-    }
-  }, [])
 
   return (
     <section className={clsx(s.wrapper, className)}>
@@ -58,19 +48,22 @@ export const Quality: React.FC<Props> = ({ className }) => {
       <div className='container'>
         <div className={s.inner}>
           <div className={s.head}>
-            <Stars className={s.stars} quantity={starsQuantity} width={20} />
+            <Stars className={s.stars} quantity={3} width={20} />
             <Heading className={s.title} level='h2'>
               Наши <span className=''>стандарты</span>
             </Heading>
-            <Stars className={s.stars} quantity={starsQuantity} width={20} />
+            <Stars className={s.stars} quantity={3} width={20} />
+            <Stars className={s.starsMobile} quantity={5} width={20} />
           </div>
           <div className={s.subtitle}>
             Мы следим за каждым этапом – от сбора до доставки, чтобы наши яблоки сохраняли свежесть, вкус и товарный
             вид. Современные технологии хранения и строгий контроль качества гарантируют, что вы получите только лучшие
             фрукты.
           </div>
-          <VideoPlayer className={s.videoPlayer} src='/videos/video-1.mp4' />
-          <Advantages className={s.advantages} items={advantageItems} layout='row' />
+          <div className={s.grid}>
+            <VideoPlayer className={s.videoPlayer} src='/videos/video-1.mp4' />
+            <Advantages className={s.advantages} items={advantageItems} layout='column' />
+          </div>
         </div>
       </div>
     </section>
