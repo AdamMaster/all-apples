f69b0244712e9fc1182a38878f48bb90dd172e94
AdamMaster <lampezhev86@gmail.com>
2025-02-06 20:25:04 +0300

    добавил свайп

--- src/components/mobile-menu/mobile-menu.tsx
+++ src/components/mobile-menu/mobile-menu.tsx
@@ -7,6 +7,7 @@ import { mail, messengers, phoneNumbers } from '@/shared/constants/constants'
 import { ContactLink, Logo } from '@/components/ui'
 import { Navbar } from '../navbar/navbar'
 import { X } from 'lucide-react'
+import { useGesture } from '@use-gesture/react'
 
 export const MobileMenu = () => {
   const { isOpen, setOpen } = useStoreMobileMenu()
@@ -21,25 +22,43 @@ export const MobileMenu = () => {
     }
   }, [isOpen])
 
+  const bind = useGesture({
+    onDrag: ({ direction, movement: [, my] }) => {
+      if (isOpen && direction[1] < 0 && my < -50) {
+        setOpen(false)
+      }
+    }
+  })
+
   return (
-    <div className={`${s.wrapper} ${isOpen ? s.active : ''}`}>
-      <Logo className={s.logo} />
-      <div className={s.closeButton} onClick={() => setOpen(false)}>
-        <X width={30} height={30} />
-      </div>
-      <div className={s.inner}>
-        <div className={s.menu}>
-          <Navbar mobile className={s.menu} />
-          <div className={s.links}>
-            <ContactLink text={phoneNumbers[0].text} link={`tel:${phoneNumbers[0].link}`} />
-            <ContactLink link={`mailto:${mail}`} text={mail} />
+    <div>
+      {isOpen && (
+        <div {...bind()} className={`${s.wrapper} ${isOpen ? s.active : ''}`}>
+          {/* Контент меню */}
+
+          <Logo className={s.logo} />
+          <div className={s.closeButton} onClick={() => setOpen(false)}>
+            <X width={30} height={30} />
           </div>
-          <div className={s.messengers}>
-            <ContactLink className={s.link} link={`https://wa.me/${messengers.whatsapp}`} iconId={'whatsapp-fill'} />
-            <ContactLink className={s.link} link={`https://t.me/${messengers.telegram}`} iconId={'telegram-fill'} />
+          <div className={s.inner}>
+            <div className={s.menu}>
+              <Navbar mobile className={s.menu} />
+              <div className={s.links}>
+                <ContactLink text={phoneNumbers[0].text} link={`tel:${phoneNumbers[0].link}`} />
+                <ContactLink link={`mailto:${mail}`} text={mail} />
+              </div>
+              <div className={s.messengers}>
+                <ContactLink
+                  className={s.link}
+                  link={`https://wa.me/${messengers.whatsapp}`}
+                  iconId={'whatsapp-fill'}
+                />
+                <ContactLink className={s.link} link={`https://t.me/${messengers.telegram}`} iconId={'telegram-fill'} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
