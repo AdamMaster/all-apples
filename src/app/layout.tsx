8c138a7e21dac959c075a3266c03ac93d6ed5e11
AdamMaster <lampezhev86@gmail.com>
2025-05-06 22:41:16 +0300

    поправил шрифты

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,23 +8,23 @@ import NextTopLoader from 'nextjs-toploader'
 const mazzard = localFont({
   src: [
     {
-      path: '../../public/fonts/MazzardH-Light.woff',
+      path: '../../public/fonts/MazzardH-Light.woff2',
       weight: '300'
     },
     {
-      path: '../../public/fonts/MazzardH-Regular.woff',
+      path: '../../public/fonts/MazzardH-Regular.woff2',
       weight: '400'
     },
     {
-      path: '../../public/fonts/MazzardH-Medium.woff',
+      path: '../../public/fonts/MazzardH-Medium.woff2',
       weight: '500'
     },
     {
-      path: '../../public/fonts/MazzardH-SemiBold.woff',
+      path: '../../public/fonts/MazzardH-SemiBold.woff2',
       weight: '600'
     },
     {
-      path: '../../public/fonts/MazzardH-Bold.woff',
+      path: '../../public/fonts/MazzardH-Bold.woff2',
       weight: '700'
     }
   ],
