05ded9c6a4fca826beae8d68925770935ca32ae9
AdamMaster <lampezhev86@gmail.com>
2025-05-10 13:58:33 +0300

    упростил компонент Heading

--- src/components/ui/heading/heading.tsx
+++ src/components/ui/heading/heading.tsx
@@ -7,22 +7,6 @@ interface Props {
 }
 
 export const Heading: FC<Props> = ({ level, children, className }) => {
-  const renderingElement = () => {
-    switch (level) {
-      case 'h1':
-        return <h1 className={className ? className : ''}>{children}</h1>
-      case 'h2':
-        return <h2 className={className ? className : ''}>{children}</h2>
-      case 'h3':
-        return <h3 className={className ? className : ''}>{children}</h3>
-      case 'h4':
-        return <h4 className={className ? className : ''}>{children}</h4>
-      case 'h5':
-        return <h5 className={className ? className : ''}>{children}</h5>
-      case 'h6':
-        return <h6 className={className ? className : ''}>{children}</h6>
-    }
-  }
-
-  return <React.Fragment>{renderingElement()}</React.Fragment>
+  const Tag = level as keyof JSX.IntrinsicElements
+  return <Tag className={className}>{children}</Tag>
 }
