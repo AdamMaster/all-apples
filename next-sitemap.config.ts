0e1b78dc858d640c805e378100a561ab6995515b
AdamMaster <lampezhev86@gmail.com>
2025-06-06 00:30:16 +0300

    снова обновил sitemap

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -3,12 +3,13 @@ import type { IConfig } from 'next-sitemap'
 const config: IConfig = {
   siteUrl: 'https://allapples.ru',
   generateRobotsTxt: true,
-  changefreq: 'weekly',
   sitemapSize: 5000,
+  changefreq: 'weekly',
+  exclude: ['/api/*'],
   transform: async (config, path) => {
     return {
       loc: path,
-      changefreq: 'weekly',
+      changefreq: path === '/' ? 'daily' : 'weekly',
       priority: path === '/' ? 1 : 0.7,
       lastmod: new Date().toISOString()
     }
