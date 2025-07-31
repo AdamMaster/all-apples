304da17173ee831525c3e49546d2581cf67f53bd
AdamMaster <lampezhev86@gmail.com>
2025-07-31 17:24:57 +0300

    добавил редирект в файл next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,22 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false
+	reactStrictMode: false,
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'www.allapples.ru',
+					},
+				],
+				destination: 'https://allapples.ru/:path*',
+				permanent: true,
+			},
+		]
+	},
 };
 
 export default nextConfig;
