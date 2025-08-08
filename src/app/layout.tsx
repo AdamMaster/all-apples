772b2f3c25147097adf59975558a0fab277b3f28
AdamMaster <lampezhev86@gmail.com>
2025-08-08 18:44:58 +0300

    Добавил jsonLd в head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local'
 import { GoogleAnalytics, YandexMetric } from '@/components/analytics'
 import './globals.css'
 import NextTopLoader from 'nextjs-toploader'
+import Script from 'next/script'
 
 const mazzard = localFont({
   src: [
@@ -34,6 +35,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'AllApples',
+    url: 'https://allapples.ru',
+    logo: 'https://allapples.ru/images/logo.png',
+    contactPoint: [
+      {
+        '@type': 'ContactPoint',
+        telephone: '+7-930-408-79-71',
+        contactType: 'sales',
+        areaServed: 'RU',
+        availableLanguage: 'Russian'
+      }
+    ]
+  }
+
   return (
     <html lang={'ru'}>
       <head>
@@ -41,6 +59,12 @@ export default function RootLayout({
         <link rel='icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' sizes='180x180' />
+        <Script
+          id='jsonld-organization'
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          strategy='afterInteractive'
+        />
       </head>
       <body className={mazzard.className}>
         <NextTopLoader color='var(--c-yellow)' />
